b5835105da88453b41a38f575fc9e3d9e0d632b7
abdullahliaqat5386 <135427225+abdullahliaqat5386@users.noreply.github.com>
2023-06-06 23:00:24 +0500

    Add files via upload

---
+++ templates.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+template <class T>
+class vector
+{
+    T *arr;
+    int size;
+};
+ 
+int main()
+{
+    vector<int> v1();
+    vector<float> v2();
+}
+
